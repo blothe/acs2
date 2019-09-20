3d1e8cb0df95d4f35386beb3bedfe0f8f0f37804
blothe <b.lothe@codeur.online>
2019-09-20 21:08:49 +0200

    Separate files in order to create a theme structure ; add content and custom menu

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+function wpdocs_theme_name_scripts() {
+  wp_register_style('main-style', get_template_directory_uri().'/blog.css', array(), true);
+  wp_enqueue_style('main-style');
+  wp_register_style('bootstrap-style', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), true);
+  wp_enqueue_style('bootstrap-style');
+}
+add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
+
+register_nav_menus( array(
+  'menu-principal' => 'Menu principal'
+) );
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="blothe">
+  <title><?php echo get_bloginfo( 'name' ); ?></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+  <link href="https://fonts.googleapis.com/css?family=Gentium+Book+Basic&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Yeseva+One&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/png" href="img/favlambda.png">
+  <link href="<?php bloginfo('template_directory');?>/style.css" rel="stylesheet">
+  <!-- HTML5 shim et Respond.js pour supporter les éléments HTML5 pour Internet Explorer 8 -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <?php wp_head(); ?>
+</head>
+<body>
